5bc197ebe7ef366ce76a8bdd5523337dee2984b6
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-06-23 21:03:44 -0300

    Updates

---
+++ precache-manifest.e95e9de32a2deb3dd8d5ac21f90f0ee7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d29d0db8ed68f586f9f19787e5c5f3a",
+    "url": "/pagina-pessoal/index.html"
+  },
+  {
+    "revision": "d9ca7129c2bb3e3761d4",
+    "url": "/pagina-pessoal/static/css/main.3680e01b.chunk.css"
+  },
+  {
+    "revision": "68c9c53bec6364fbca0e",
+    "url": "/pagina-pessoal/static/js/2.af2726b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pagina-pessoal/static/js/2.af2726b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ca7129c2bb3e3761d4",
+    "url": "/pagina-pessoal/static/js/main.84209ebc.chunk.js"
+  },
+  {
+    "revision": "eb3dc3b45544192568a0",
+    "url": "/pagina-pessoal/static/js/runtime-main.053f00c1.js"
+  },
+  {
+    "revision": "e9fcb24d2baf2342285f92b6b940762e",
+    "url": "/pagina-pessoal/static/media/bootstrap-logo.e9fcb24d.png"
+  },
+  {
+    "revision": "c6fd6eb55dcaf1e3c56b29610272b56d",
+    "url": "/pagina-pessoal/static/media/coffeelover.c6fd6eb5.jpeg"
+  },
+  {
+    "revision": "6013be9449eb5efe0645a266e8094819",
+    "url": "/pagina-pessoal/static/media/css-logo2.6013be94.png"
+  },
+  {
+    "revision": "90bd5c3a0e2ffbcf6cdd89bf13077f5d",
+    "url": "/pagina-pessoal/static/media/ecoleta.90bd5c3a.jpeg"
+  },
+  {
+    "revision": "23082b66fbab564617e33c7ef8222495",
+    "url": "/pagina-pessoal/static/media/foto-leonardorendano.23082b66.jpg"
+  },
+  {
+    "revision": "8d0bf56efa8e57b28aaebd35dfc760a8",
+    "url": "/pagina-pessoal/static/media/git-logo3.8d0bf56e.jpg"
+  },
+  {
+    "revision": "50a10d1ce2cfde88b6ebfdaeea4fc0b3",
+    "url": "/pagina-pessoal/static/media/html-logo2.50a10d1c.png"
+  },
+  {
+    "revision": "b08f5b342afbec2f40485ae9120d002d",
+    "url": "/pagina-pessoal/static/media/javascript-logo.b08f5b34.png"
+  },
+  {
+    "revision": "dbb5e3f0bd2f54c3d96ce80e5f3a356f",
+    "url": "/pagina-pessoal/static/media/left-bracket.dbb5e3f0.jpg"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/pagina-pessoal/static/media/react-2.876a8325.svg"
+  },
+  {
+    "revision": "2d6fa2b40957b8f139bfdddaf585ba69",
+    "url": "/pagina-pessoal/static/media/sass-logo.2d6fa2b4.png"
+  },
+  {
+    "revision": "86319dd9de9ffde71994ff341e0190db",
+    "url": "/pagina-pessoal/static/media/styled-logo.86319dd9.png"
+  }
+]);
